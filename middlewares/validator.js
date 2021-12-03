b61d6685086dd210d6ffb3b444fc2b83d04e4ba5
Austin Hill <ahill112@uncc.edu>
2021-12-03 12:23:21 -0500

    added validation

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,4 +1,6 @@
 //check if user is author
+const {body} = require('express-validator');
+const {validationResult} = require('express-validator');
 exports.validateId = (req, res, next)=>{
     let id  = req.params.id
     if(!id.match(/^[0-9a-fA-F]{24}$/)) {
@@ -10,3 +12,17 @@ exports.validateId = (req, res, next)=>{
     }
     
 };
+
+exports.validateRsvp = [body('rsvp').isIn(['YES','NO','MAYBE'])];
+
+exports.validateResults = (req, res, next) =>{
+    let errors = validationResult(req);
+    if(!errors.isEmpty()){
+        errors.array().forEach(error=>{
+            req.flash('error', error.msg);
+        });
+        return res.redirect('back');
+    } else {
+        return next();
+    }
+}
--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/connectionController');
 const {isLoggedIn, isAuthor, isNotAuthor} = require('../middlewares/auth')
-const {validateId} = require('../middlewares/validator')
+const {validateId, validateRsvp, validateResults} = require('../middlewares/validator')
 
 router.get('/', controller.index);
 
@@ -18,7 +18,7 @@ router.get('/:id/edit', isLoggedIn, validateId, isAuthor, controller.edit);
 
 router.put('/:id', isLoggedIn, validateId, isAuthor, controller.update);
 
-router.post('/:id/rsvp', validateId, isLoggedIn, isNotAuthor, controller.editRsvp);
+router.post('/:id/rsvp', validateId, isLoggedIn, isNotAuthor, validateRsvp, validateResults, controller.editRsvp);
 
 router.delete('/:id/rsvp', validateId, isLoggedIn, controller.deleteRsvp);
 
